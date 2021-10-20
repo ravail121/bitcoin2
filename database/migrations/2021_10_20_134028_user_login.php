0cd39458bf7f90bf2efff3cf6d742194503bbe4c
ravail121 <rvlirshad@gmail.com>
2021-10-20 14:36:24 +0500

    2:36

---
+++ database/migrations/2021_10_20_134028_user_login.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UserLogin extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_login', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->foreign('user_id')
+                  ->references('id')->on('users')
+                  ->onDelete('cascade');
+            $table->string('location')->nullable();
+            $table->string('user_ip')->nullable();
+            $table->string('country_name')->nullable();
+            $table->string('details')->nullable();
+            $table->string('browser')->nullable();
+            $table->string('platform')->nullable();
+            $table->string('action')->nullable();
+            $table->integer('is_country_changed')->nullable();
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
